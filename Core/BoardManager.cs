3217bf139fb551f9142dd3353843d5a13a805e9f
qwfmok <pdnrdl2@yonsei.ac.kr>
2026-05-11 09:54:37 +0900

    Add basic file structure

---
+++ Core/BoardManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardChess.Core
+{
+    internal class BoardManager
+    {
+    }
+}
---
+++ Pieces/IPieces.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardChess.Models;
+
+namespace CardChess.Pieces
+{
+    internal interface IPieces
+    {
+        Position CurrentPosition { get; set; }
+        PlayerType Owner { get; }
+        PieceType Type { get; }
+
+        // 이동 가능한 모든 좌표 반환
+        List<Position> GetMovablePositions(GameState state);
+
+        // 공격 가능한 모든 좌표 반환 (폰의 경우 이동과 공격 범위가 다름)
+        List<Position> GetAttackablePositions(GameState state);
+
+        // 특정 위치로 이동/공격이 가능한지 최종 확인
+        bool CanMove(Position target, GameState state);
+        bool CanAttack(Position target, GameState state);
+    }
+}
